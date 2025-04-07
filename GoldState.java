d9e892eb937cfd904dae58910c6929f629581324
Meet Patadia <madhavrpatadia@gmail.com>
2025-04-07 15:35:13 -0400

    Create GoldState.java

---
+++ GoldState.java
@@ -0,0 +1,27 @@
+package bookstore;
+
+/**
+ *
+ * @author a34hasan
+ */
+public class GoldState extends CustomerState {
+    public GoldState(Customer customer) {
+        super(customer);
+    }
+
+    @Override
+    public void buyBook(String bookName, double price) {
+        customer.addPoints((int)(price * 10)); //Adds 10 points per dollar spent on book
+    }
+
+    @Override
+    public void redeemPoints(String bookName, int points) {
+        if (customer.getPoints() >= points) {
+            customer.addPoints(-points);
+            
+            if (customer.getPoints() < 1000) {
+                customer.setState(new SilverState(customer));
+            }
+        }
+    }
+}
